772410eaf4739a8718b4f5316a603dd626a9e393
Noe Duran <durann031@gmail.com>
2022-02-17 23:10:05 -0500

    Initial Commit

---
+++ Shared/RUBussinApp.swift
@@ -0,0 +1,17 @@
+//
+//  RUBussinApp.swift
+//  Shared
+//
+//  Created by Noé Duran on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct RUBussinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
