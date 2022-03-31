ca6c2250c8c14f8d383dc9fc795d9b72d807d91d
Noe Duran <durann031@gmail.com>
2022-03-30 21:22:01 -0400

    Test

--- Shared/BusListView.swift
+++ Shared/BusListView.swift
@@ -10,9 +10,7 @@ import SwiftUI
 struct BusListView: View {
     var body: some View {
         List{
-            Text("Hello, World!")
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            //Input stuff
         }
     }
 }
